e5d566f630d85dc5e93cefb66029ec843c593a0b
Anshulguptag <anshulguptamasteramazon@gmail.com>
2018-05-25 02:54:58 +0530

    Ecommerce

---
+++ admin_area/includes/db.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$con = mysqli_connect("localhost","root","Anshul@13","ecommerce");
+
+if(mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: ".mysqli_connect_errno();
+}
+
+?>
